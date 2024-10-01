1f043e60db13b05ef7767ed159771bed288daec2
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2024-10-01 16:32:05 +0200

    use dynamorio backend

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Allow to profile a function with the TMAM (Top Down Analysis Methodology) thanks to DBI (Dynamic Binary Instrumentation).
 
 # Requirements
- - QDBI (https://github.com/QBDI/QBDI)
+ - DynamoRIO >= 10.93.19987 (https://github.com/DynamoRIO/dynamorio) -- See releases to get the latest version
  - libelf (Arch: libelf, Debian: libelf-dev)
-
-See the QBDI documentation for more information.
 
 # Build
 ```bash
-cmake -B build
+wget <DynamoRIO_URL> -O <DynamoRIO_TAR>
+tar -xf <DynamoRIO_TAR> -C <Path>
+cmake -DDynamoRIO_DIR=<Path> -B build
 make -C build -j 8
 ```
 
@@ -21,15 +21,18 @@ make -C build -j 8
  - **Perf** It can be `TopdownL1`, `TopdownL2`, `TopdownL3`, `TopdownL3`.
  - **Toplev**: `1` for L1, `2` for L2, `3` for L3
 - `TMA_TOPLEV` controls whether to use the toplev tool or not. The value of the argument will be the path to the `toplev` tool.
+- `TMA_CORE` is the core where the analysis will be performed. (Mandatory)
+
+```bash
 
 Note that sampling tools require a more-or-less large number of samples to be accurate. Hence, you should repeat your workload enough times to get a meaningful output.
 
 For a simple example using `perf`:
 ```bash
-TMA_FUNCTION=kernel TMA_OUTPUT_FILE=tma.txt TMA_LEVEL=TopdownL1 LD_PRELOAD=./libtmascope.so {YOUR_PROGRAM}
+TMA_FUNCTION=kernel TMA_OUTPUT_FILE=tma.txt TMA_LEVEL=TopdownL1 TMA_CORE=0 TMA<PATH>/bin64/drrun -c ?/libtmascope.so -- {YOUR_PROGRAM}
 ```
 
 Same example using `toplev` considering that `pmu-tools` is the path to the `toplev` tool:
 ```bash
-TMA_FUNCTION=kernel TMA_OUTPUT_FILE=toplev3.txt TMA_LEVEL=1 TMA_TOPLEV=pmu-tools LD_PRELOAD=./libtmascope.so {YOUR_PROGRAM}
+TMA_FUNCTION=kernel TMA_OUTPUT_FILE=toplev3.txt TMA_LEVEL=1 TMA_CORE=0 TMA_TOPLEV=pmu-tools <PATH>/bin64/drrun -c ./libtmascope.so -- {YOUR_PROGRAM}
 ```
--- src/perf.cpp
+++ src/perf.cpp
@@ -1,12 +1,15 @@
-#include "QBDI.h"
-#include "QBDIPreload.h"
-#include "elf.hpp"
-#include "proc.hpp"
+#include "dr_api.h"
+#include "dr_events.h"
+#include "drmgr.h"
+#include "drsyms.h"
+#include "drwrap.h"
 
 #include <iostream>
 #include <optional>
 #include <cstddef>
 #include <algorithm>
+#include <vector>
+#include <functional>
 
 #include <unistd.h>
 #include <dlfcn.h>
@@ -17,69 +20,22 @@
 #include <unistd.h>
 #include <signal.h>
 
-struct stack_s
-{
-  QBDI::rword returnAddress;
-};
+//Imports
+#include "dr_api.h"
+#include "dr_events.h"
+#include "drmgr.h"
+#include "drsyms.h"
+#include "drwrap.h"
+#include <stdint.h>
 
 /**
- * @brief Get the address of the the symbol in the specified process
- *
- * @param pid Process ID
- * @param name Unmangled symbol name
- * @return Memory address (**Note: this is sensitive because it would allow an attacker to bypass
- * ASLR if leaked!**)
+ * @brief Target function post-execution handler
+ * 
+ * @param ctx DynamoRIO context
+ * @param data User data
  */
-static QBDI::rword getAddress(pid_t pid, std::string name)
+static void onAfterTarget(void *ctx, void *data)
 {
-  // Get the executable path
-  std::string path = getPath(pid);
-
-  // Get all symbols
-  std::vector symbols = getSymbols(path);
-
-  // Get the symbol address
-  QBDI::rword symbolAddress = 0;
-  QBDI::rword symbolEndAddress = 0;
-  for (Symbol symbol : symbols)
-  {
-    if (symbol.unmangledName == name)
-    {
-      symbolAddress = symbol.address;
-      symbolEndAddress = symbol.address + symbol.size;
-      break;
-    }
-  }
-
-  if (symbolAddress == 0)
-  {
-    throw std::runtime_error("Failed to get symbol address!");
-  }
-
-  // Get all memory map entries
-  std::vector<QBDI::MemoryMap> entries = QBDI::getCurrentProcessMaps(true);
-
-  // Get the base address
-  QBDI::rword baseAddress = 0;
-  for (QBDI::MemoryMap entry : entries)
-  {
-    // Check if the paths match
-    if (entry.name == path && (entry.permission & QBDI::PF_EXEC) == 0)
-    {
-      baseAddress = entry.range.start();
-      break;
-    }
-  }
-
-  if (baseAddress == 0)
-  {
-    throw std::runtime_error("Failed to get base address!");
-  }
-
-  // Compute the full address
-  QBDI::rword fullAddress = baseAddress + symbolAddress;
-
-  return fullAddress;
 }
 
 void child_run(const std::vector<std::string>& cmd) {
@@ -102,19 +58,19 @@ void vfork_child_run(const std::vector<std::string>& cmd, std::optional<std::fun
     }
 }
 
-static QBDI::VMAction onTargetEnd(QBDI::VMInstanceRef vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data)
+/**
+ * @brief Target function pre-execution handler
+ * 
+ * @param ctx DynamoRIO context
+ * @param data User data
+ */
+static void onBeforeTarget(void *ctx, void **data)
 {
-  return QBDI::STOP;
-}
-
-static QBDI::VMAction onTarget(QBDI::VMInstanceRef vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data)
-{
-  #if defined(QBDI_ARCH_X86_64)
-  stack_s* stack = reinterpret_cast<stack_s*>(gprState->rsp);
-  vm->addCodeAddrCB((QBDI::rword)stack->returnAddress, QBDI::PREINST, onTargetEnd, nullptr);
-  #endif
-
   pid_t pid = getpid();
+
+  dr_printf("Before target: %d\n", pid);
+
+  std::string tma_core = getenv("TMA_CORE");
 
   vfork_child_run({"/usr/bin/taskset", "--pid", "--cpu-list", "0", std::to_string(pid)}, [&]() {
     std::string tma_output_file = getenv("TMA_OUTPUT_FILE");
@@ -123,111 +79,98 @@ static QBDI::VMAction onTarget(QBDI::VMInstanceRef vm, QBDI::GPRState *gprState,
       vfork_child_run({"/usr/bin/perf", "stat", "-M", tma_level, "-p", std::to_string(pid), "-o", tma_output_file});
     } else {
       std::string tma_toplev = getenv("TMA_TOPLEV");
-      vfork_child_run({"/usr/bin/python", tma_toplev + "/toplev.py", "--single-thread", "-l", tma_level, "-o", tma_output_file, "--pid", std::to_string(pid), "--core", "C0"});
+      vfork_child_run({"/usr/bin/python", tma_toplev + "/toplev.py", "--single-thread", "-l", tma_level, "-o", tma_output_file, "--pid", std::to_string(pid), "--core", "C"+tma_core});
     }
 
   });
-
-  return QBDI::CONTINUE;
 }
 
-extern "C"
+/**
+ * @brief DynamoRIO module loaded handler
+ * 
+ * @param ctx DynamoRIO context
+ * @param module Module that was loaded
+ * @param loaded Whether or not the module was loaded?
+ */
+static void onModuleLoad(void *ctx, const module_data_t *module, bool loaded)
 {
-  /**
-   * @brief QBDIPreload constructor
-   *
-   */
-  QBDIPRELOAD_INIT;
-
-  /**
-   * @brief QBDIPreload program entrypoint handler
-   *
-   * @param main Main function address
-   * @return QBDIpreload state
-   */
-  int qbdipreload_on_start(void *main)
+  const char *tma_function_name = getenv("TMA_FUNCTION");
+  if (tma_function_name == NULL)
   {
-    return QBDIPRELOAD_NOT_HANDLED;
+    printf("TMA_FUNCTION not set\n");
+    exit(1);
   }
 
-  /**
-   * @brief QBDIPreload main function preload handler
-   *
-   * @param gprCtx Original General Purpose Registers (GPR) context
-   * @param fpuCtx Original Floating Point Registers (FPR) context
-   * @return QBDIpreload state
-   */
-  int qbdipreload_on_premain(void *gprCtx, void *fprCtx)
+  const char *tma_core = getenv("TMA_CORE");
+  if (tma_core == NULL)
   {
-    return QBDIPRELOAD_NOT_HANDLED;
+    printf("TMA_CORE not set\n");
+    exit(1);
   }
 
-  /**
-   * @brief QBDIPreload main function hijacked handler
-   *
-   * @param argc Argument count
-   * @param argv Argument values
-   * @return QBDIpreload state
-   */
-  int qbdipreload_on_main(int argc, char *argv[])
+  const char *tma_output_file = getenv("TMA_OUTPUT_FILE");
+  if (tma_output_file == NULL)
   {
-    return QBDIPRELOAD_NOT_HANDLED;
+    printf("TMA_OUTPUT_FILE not set\n");
+    exit(1);
   }
 
-  /**
-   * @brief QBDIPreload main function ready-to-run handler
-   *
-   * @param vm QBDIPreload virtual machine
-   * @param start Start address
-   * @param stop Stop address
-   * @return QBDIpreload state
-   */
-  int qbdipreload_on_run(QBDI::VMInstanceRef vm, QBDI::rword start, QBDI::rword stop)
+  const char *tma_level = getenv("TMA_LEVEL");
+  if (tma_level == NULL)
   {
-    // Get own pid
-    pid_t pid = getpid();
-
-    const char *tma_function_name = getenv("TMA_FUNCTION");
-    if (tma_function_name == NULL)
-    {
-      printf("TMA_FUNCTION not set\n");
-      return QBDIPRELOAD_ERR_STARTUP_FAILED;
-    }
-
-    const char *tma_output_file = getenv("TMA_OUTPUT_FILE");
-    if (tma_output_file == NULL)
-    {
-      printf("TMA_OUTPUT_FILE not set\n");
-      return QBDIPRELOAD_ERR_STARTUP_FAILED;
-    }
-
-    const char *tma_level = getenv("TMA_LEVEL");
-    if (tma_level == NULL)
-    {
-      printf("TMA_LEVEL not set\n");
-      return QBDIPRELOAD_ERR_STARTUP_FAILED;
-    }
-
-    // //Get the target address
-    auto targetAddress = getAddress(pid, tma_function_name);
-
-    // Register callbacks
-    vm->addCodeAddrCB((QBDI::rword)targetAddress, QBDI::PREINST, onTarget, NULL);
-
-    // Run
-    vm->run(start, stop);
-
-    return QBDIPRELOAD_NO_ERROR;
+    printf("TMA_LEVEL not set\n");
+    exit(1);
   }
 
-  /**
-   * @brief QBDIPreload program exit handler handler
-   *
-   * @param status Exit status
-   * @return QBDIpreload state
-   */
-  int qbdipreload_on_exit(int status)
+  //Get the target function offset
+  size_t offset;
+  drsym_error_t error = drsym_lookup_symbol(module->full_path, tma_function_name, &offset, DRSYM_LEAVE_MANGLED);
+
+  //Get the target function address
+  app_pc address = module->start + offset;
+
+  if (error == DRSYM_SUCCESS)
   {
-    return QBDIPRELOAD_NO_ERROR;
+    printf("Symbol found: %s\n", tma_function_name);
+    //Wrap the target function
+    drwrap_wrap(address, onBeforeTarget, onAfterTarget);
+  } else {
+    printf("Symbol not found: %s\n", tma_function_name);
   }
+}
+
+/**
+ * @brief DynamoRIO exit handler
+ */
+static void onExit()
+{
+  //Cleanup
+  drsym_exit();
+  drwrap_exit();
+  drmgr_exit();
+}
+
+/**
+ * @brief DynamoRIO entry point
+ * 
+ * @param id Client ID
+ * @param argc Argument count
+ * @param argv Argument values
+ */
+DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
+{
+  //Update client metadata
+  dr_set_client_name("tmascope", "https://github.com/adutilleul/tma-scope");
+
+  //Initialize DynamoRIO
+  drmgr_init();
+  drwrap_init();
+  drsym_init(0);
+
+  //Register handlers
+  dr_register_exit_event(onExit);
+  drmgr_register_module_load_event(onModuleLoad);
+
+  //Configure function wrapping (Improve performance)
+  drwrap_set_global_flags(static_cast<drwrap_global_flags_t>(DRWRAP_NO_FRILLS | DRWRAP_FAST_CLEANCALLS));
 }
